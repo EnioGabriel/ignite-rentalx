bb0d21ac29d7c026345a95b199f97c2a52612904
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-08-27 01:12:36 -0300

    CRiando os use case de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,15 @@
 import { Router } from "express";
+
+import { createCategoryController } from "../modules/cars/model/useCases/createCategory";
 import CategoriesRepository from "../modules/cars/repositories/CategoriesRepository";
-import CreateCategoryService from "../modules/cars/services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 // Instanciando objeto
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
-  const { name, description } = req.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-
-  return res.status(201).send();
+  // chamando controller responsavel pelas requests e responses
+  return createCategoryController.handle(req, res);
 });
 
 categoriesRoutes.get("/", (request, response) => {
