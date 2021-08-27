b36f85b61a3c0b190145245d62f1bd470f4b2ef8
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-08-26 21:42:34 -0300

    Criando repositório de especificação

--- src/modules/cars/services/CreateSpecificationServices.ts
+++ src/modules/cars/services/CreateSpecificationServices.ts
@@ -1,5 +1,25 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequestDTO {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {}
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+  execute({ name, description }: IRequestDTO): void {
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Essa especificação já existe!");
+    }
+
+    this.specificationsRepository.create({
+      name,
+      description,
+    });
+  }
 }
 
 export { CreateSpecificationService };
