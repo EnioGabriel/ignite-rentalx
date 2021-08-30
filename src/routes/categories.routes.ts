a50a6e0d4b76675ea849359f9348fe8f339b020e
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-08-30 16:13:46 -0300

    Criando upload de arquivos (multer)

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+
+// Recebe o arquivo e armazena em uma pasta temp
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (req, res) => {
   // chamando controller responsavel pelas requests e responses
@@ -13,6 +19,14 @@ categoriesRoutes.post("/", (req, res) => {
 categoriesRoutes.get("/", (request, response) => {
   // chamando controller responsavel pelas requests e responses
   return listCategoriesController.handle(request, response);
+});
+
+// Rota para receber upload de arquivo
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  const { file } = request;
+  console.log(file);
+
+  response.send();
 });
 
 export { categoriesRoutes };
