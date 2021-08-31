47b358cb03f3d82063d28a995a248d32369263b5
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-08-31 18:35:08 -0300

    Aplicando conceito stream de arquivos

--- src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
+++ src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
@@ -1,6 +1,19 @@
+import fs from "fs";
+import csvParse from "csv-parse";
+
 class ImportCategoriesUseCase {
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File) {
+    // Lê arquivo
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    // pipe => funciona igual conceito de buffer em videos (pega pedaços do arquivo e vai repassando)
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
