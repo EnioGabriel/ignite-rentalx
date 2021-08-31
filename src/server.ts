5a8a7c2e2acc5d06b741c6b6ce3417b35d29dd89
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-08-31 19:33:12 -0300

    Configurando o swagger

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import { router } from "./routes";
+import swaggerUi from "swagger-ui-express";
+
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
+// Habilita o uso de .json em rotas
 app.use(express.json());
 
 // Chamando o arquivo index.ts, onde está especificando as rotas
 app.use(router);
+
+// Swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => {
   console.log("🚀️ Server started");
