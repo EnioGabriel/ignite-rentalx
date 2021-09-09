72ca6b59123cecd96a0e2adfdb1393b8b32415ff
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-09-09 02:27:35 -0300

    Criptografar senha e impedindo usuário com email já cadastrado

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,6 @@
 import { inject, injectable } from "tsyringe";
+import { hash } from "bcrypt";
+
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -15,10 +17,21 @@ class CreateUserUseCase {
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+    // Verificando se já possui um usuário com mesmo email
+    const userAlreadyExists = this.userRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    // criptografando senha com hash
+    const passwordHash = await hash(password, 8);
+
+    // Passando dados pro repositório responsavel por salvar no BD
     await this.userRepository.create({
       name,
       email,
-      password,
+      password: passwordHash,
       driver_license,
     });
   }
