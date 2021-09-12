9e86896c53292a182b80f059f5e0203d17d6bd5a
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-09-12 15:45:54 -0300

    Upload de avatar

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,6 +1,24 @@
+// Responsável por controlar o upload de arquivos
+import crypto from "crypto";
 import multer from "multer";
 import { resolve } from "path";
 
 export default {
-  upload();,
+  upload(folder: string) {
+    return {
+      storage: multer.diskStorage({
+        destination: resolve(__dirname, "..", "..", folder),
+        filename: (request, file, callback) => {
+          // hash para gerar sempre um num aleatório e evitar repetiçao de nome
+          const fileHash = crypto.randomBytes(16).toString("hex");
+          // Concatenando filehash com o nome do arquivo enviado
+          const fileName = `${fileHash}-${file.originalname}`;
+
+          // 1° param: erro
+          // 2° param: arquivo
+          return callback(null, fileName);
+        },
+      }),
+    };
+  },
 };
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,14 +1,27 @@
 import { Router } from "express";
+import multer from "multer";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middleware/ensureAuthenticated";
 
 const usersRoutes = Router();
+
+// Envia caminho para o controlador de upload
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserController.handle);
 
-usersRoutes.patch("/avatar", updateUserAvatarController.handle);
+usersRoutes.patch(
+  "/avatar",
+  // middleware resposavel pela autenticação
+  ensureAuthenticated,
+  // middleware para upload de avatar
+  uploadAvatar.single("avatar"),
+  updateUserAvatarController.handle
+);
 
 export { usersRoutes };
