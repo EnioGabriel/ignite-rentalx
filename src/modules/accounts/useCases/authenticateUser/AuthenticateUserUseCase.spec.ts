85a47efbb10dd35934fdfa1534af80cf67d57558
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-09-15 01:58:30 -0300

    Teste de autenticação de usuário

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,66 @@
+import { AppError } from "../../../../errors/AppError";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Authenticate user", () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    authenticateUserUseCase = new AuthenticateUserUseCase(
+      usersRepositoryInMemory
+    );
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+  });
+
+  it("Should be able to authenticate a user", async () => {
+    const user: ICreateUserDTO = {
+      name: "User Test",
+      email: "email@example.com",
+      password: "passwordTest",
+      driver_license: "12345",
+    };
+
+    // Criando usuário
+    await createUserUseCase.execute(user);
+
+    const result = await authenticateUserUseCase.execute({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(result).toHaveProperty("token");
+  });
+
+  it("Should not be able to authenticate a nonexistent user", async () => {
+    expect(async () => {
+      await authenticateUserUseCase.execute({
+        email: "fakemail@example.com",
+        password: "fakePassword",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("Should not be able to authenticate with incorrect password", async () => {
+    expect(async () => {
+      const user: ICreateUserDTO = {
+        name: "User Test",
+        email: "email@example.com",
+        password: "passwordTest",
+        driver_license: "12345",
+      };
+
+      // Criando usuário
+      await createUserUseCase.execute(user);
+
+      await authenticateUserUseCase.execute({
+        email: "email@example.com",
+        password: "wrongPassword",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
