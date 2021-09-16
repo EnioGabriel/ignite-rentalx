aa86a6188c58151f5488407adeba466f53df232a
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-09-16 15:20:18 -0300

    Listando carros disponíveis

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -33,6 +33,28 @@ class CarsRepositoryInMemory implements ICarsRepository {
   async findByLicensePlate(license_plate: String): Promise<Car> {
     return this.cars.find((car) => car.license_plate === license_plate);
   }
+
+  // Retorna todos os carros com disponibilidade para aluguel
+  async findAvailable(
+    brand?: string,
+    category_id?: string,
+    name?: string
+  ): Promise<Car[]> {
+    const all = this.cars.filter((car) => {
+      if (
+        car.available === true &&
+        // Verifica se está preenchido e se é igual ao valor passado
+        ((brand && car.brand === brand) ||
+          (category_id && car.category_id === category_id) ||
+          (name && car.name === name))
+      ) {
+        return car;
+      }
+      return null;
+    });
+
+    return all;
+  }
 }
 
 export { CarsRepositoryInMemory };
