bd4e22604c3a16d98cb25ce9b5ffce95831129df
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-09-16 16:50:40 -0300

    Criando migrations especificação de carros(Many to Many)

---
+++ src/shared/infra/typeorm/migrations/1631819028101-CreateSpecificationsCars.ts
@@ -0,0 +1,73 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from "typeorm";
+
+export class CreateSpecificationsCars1631819028101
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "specifications_cars",
+        columns: [
+          {
+            name: "car_id",
+            type: "uuid",
+          },
+          {
+            name: "specification_id",
+            type: "uuid",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+
+    // Referencia specification/car
+    await queryRunner.createForeignKey(
+      "specifications_cars",
+      new TableForeignKey({
+        name: "FKSpecificationCar",
+        referencedTableName: "specifications",
+        referencedColumnNames: ["id"],
+        columnNames: ["specification_id"],
+        onDelete: "SET NULL",
+        onUpdate: "SET NULL",
+      })
+    );
+
+    // Referencia car/specification
+    await queryRunner.createForeignKey(
+      "specifications_cars",
+      new TableForeignKey({
+        name: "FKCarSpecification",
+        referencedTableName: "cars",
+        referencedColumnNames: ["id"],
+        columnNames: ["car_id"],
+        onDelete: "SET NULL",
+        onUpdate: "SET NULL",
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(
+      "specifications_cars",
+      "FKCarSpecification"
+    );
+
+    await queryRunner.dropForeignKey(
+      "specifications_cars",
+      "FKSpecificationCar"
+    );
+
+    await queryRunner.dropTable("specifications_cars");
+  }
+}
